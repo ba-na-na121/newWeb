73023a67c80fe543b6297686b19a6c7dafcb3fa9
ba-na-na121 <xiaw902@newschool.edu>
2025-05-30 10:03:09 +0800

    lifestyleUpdate2

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA6RiB-bqTR3on_KpIRARLm0W3SWeYWaz0",
   authDomain: "wenzuoweb.firebaseapp.com",
@@ -10,4 +14,9 @@ const firebaseConfig = {
   measurementId: "G-MDDZ10S396"
 };
 
-initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firestore and Storage instances
+export const db = getFirestore(app);
+export const storage = getStorage(app);
