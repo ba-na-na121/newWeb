925d314b968b543d35c49fdb7fa02666dee5452d
ba-na-na121 <xiaw902@newschool.edu>
2025-05-30 09:55:43 +0800

    lifestylePage

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6RiB-bqTR3on_KpIRARLm0W3SWeYWaz0",
+  authDomain: "wenzuoweb.firebaseapp.com",
+  projectId: "wenzuoweb",
+  storageBucket: "wenzuoweb.appspot.com",
+  messagingSenderId: "415514650847",
+  appId: "1:415514650847:web:18d293683bfcbea4f9e16a",
+  measurementId: "G-MDDZ10S396"
+};
+
+initializeApp(firebaseConfig);
